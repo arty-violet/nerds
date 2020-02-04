f3904debb6220048bfe5479d4115fa0fcde5203e
Arty Soule <artysoule@gmail.com>
2020-02-04 22:00:18 +0300

    validator fixes

---
+++ blocks/inner-content/templates/template/template.js
@@ -0,0 +1,92 @@
+
+// var template = document.querySelector(".template");
+// var hover = document.querySelector(".template__hover");
+// var hoverShown = document.querySelector(".template__hover--shown");
+// var photoAfter1 = document.querySelector(".modal__1 .modal__photo-a");
+// var photoBefore1 = document.querySelector(".modal__1 .modal__photo-b");
+// var photoPrev1 = document.querySelector(".modal__1 .modal__button-prev");
+// var photoNext1 = document.querySelector(".modal__1 .modal__button-next");
+// var photoClose1 = document.querySelector(".modal__1 .modal__button-close");
+
+
+// hover.addEventListener("click", function (evt) {
+//   evt.preventDefault();
+//   hover.classList.add("template__hover--shown");
+// });
+
+// hover.addEventListener("mouseenter", function () {
+//   hover.classList.toggle("template__hover--shown");
+//   hover.classList.toggle("template__hover--shown");
+// });
+
+// hoverShown.addEventListener("mouseout", function () {
+//   hoverShown.classList.toggle("template__hover--shown");
+//   hoverShown.classList.toggle("template__hover");
+// });
+
+// photoAfter1.addEventListener("click", function () {
+//   photoAfter1.classList.toggle("modal__photo-a");
+//   photoAfter1.classList.toggle("modal__photo-b");
+//   photoBefore1.classList.toggle("modal__photo-b");
+//   photoBefore1.classList.toggle("modal__photo-a");
+// });
+
+// photoBefore1.addEventListener("click", function () {
+//   photoBefore1.classList.toggle("modal__photo-a");
+//   photoBefore1.classList.toggle("modal__photo-b");
+//   photoAfter1.classList.toggle("modal__photo-b");
+//   photoAfter1.classList.toggle("modal__photo-a");
+// });
+
+// photoPrev1.addEventListener("click", function(evt) {
+//   evt.preventDefault();
+//   photoPopup1.classList.remove("modal__show");
+//   photoPopup69.classList.add("modal__show");
+// })
+
+// window.addEventListener("keyup", function (evt) {
+//   if (evt.keyCode === 37) {
+//     evt.preventDefault();
+    
+//     if (photoPopup1.classList.contains("modal__show")) {
+//       setTimeout(function() {
+//         photoPopup1.classList.remove("modal__show");
+//         photoPopup69.classList.add("modal__show");
+//       }, 10);
+//     }
+//   }
+// });
+
+// photoNext1.addEventListener("click", function(evt) {
+//   evt.preventDefault();
+//   photoPopup1.classList.remove("modal__show");
+//   photoPopup2.classList.add("modal__show");
+// })
+
+// window.addEventListener("keyup", function (evt) {
+//   if (evt.keyCode === 39) {
+//     evt.preventDefault();
+
+//     if (photoPopup1.classList.contains("modal__show")) {
+//       setTimeout(function() {
+//         photoPopup1.classList.remove("modal__show");
+//         photoPopup2.classList.add("modal__show");
+//       }, 10);
+//     }
+//   }
+// });
+
+// photoClose1.addEventListener("click", function (evt) {
+//   evt.preventDefault();
+//   photoPopup1.classList.remove("modal__show");
+// });
+
+// window.addEventListener("keyup", function (evt) {
+//   if (evt.keyCode === 27) {
+//     evt.preventDefault();
+    
+//     if (photoPopup1.classList.contains("modal__show")) {
+//       photoPopup1.classList.remove("modal__show");
+//     }
+//   }
+// });
